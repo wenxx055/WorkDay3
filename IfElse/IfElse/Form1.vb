ec7cd3b26f718a64cdbf9f12cc6fba6a15564220
idsc3101 <idsc3101@gmail.com>
2013-09-18 17:23:40 -0500

    v0.5

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -1,8 +1,29 @@
-﻿Option Strict On
+﻿Option Strict On 'by Yunfan Wen, wenxx055, student ID 3938779
 
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
 
+        '-----isNumeric check-----
+        Dim FirstNumber As Double
+        Dim SecondNumber As Double
+
+        FirstNumber = CDbl(txtFirstNum.Text)
+        SecondNumber = CDbl(txtSecondNum.Text)
+
+        '-----calculation and display-----
+        If FirstNumber - SecondNumber > 0 Then
+            txtResult.Text = CStr(FirstNumber)
+        End If
+
+        If FirstNumber - SecondNumber < 0 Then
+            txtResult.Text = CStr(SecondNumber)
+        End If
+
+        If FirstNumber - SecondNumber = 0 Then
+            txtResult.Text = "The two are equal"
+        End If
+
     End Sub
+
 End Class
